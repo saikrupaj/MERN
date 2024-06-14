0f314ed23a08de6deb72bcaf0583db498dd469a6
Saikrupa Joshi <saikrupajoshi@gmail.com>
2024-06-14 11:45:42 +0530

    Variables and datatypes

---
+++ JS/1_Variables.js
@@ -0,0 +1,12 @@
+const accountID = 155443
+let emailID = "saikrupajoshi@gmail.com"
+var password = "pwd@99"
+age=25
+
+//accountID = 155441
+emailID = 11
+password = "pwd@1199"
+age=251
+
+// console.log(accountID);
+console.table([emailID,password,age])
---
+++ JS/3_DataType.js
@@ -0,0 +1,29 @@
+//alert("HI") 
+//ReferenceError: alert is not defined
+//we are using node.js not browser
+
+//---Premitive DT---
+//number 2 to power 32
+//bigInt
+//boolean
+//String
+//null (Stand Alone Value)
+//undefined
+//Symbol
+
+//---Non-Premitive DT---
+//object
+//type of 
+let name= "Sai"
+let age= 25
+let isMale= true
+let bankBal = null
+let wifeName= undefined
+
+console.log(typeof "name"); //string
+console.log(typeof age); //number
+console.log(typeof isMale); //boolean
+console.log(typeof bankBal); //object
+console.log(typeof wifeName); //undefined
+
+

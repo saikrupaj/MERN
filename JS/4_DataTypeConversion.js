5a2dade90df98cb5e6c666eb152732fa1c86c45c
Saikrupa Joshi <saikrupajoshi@gmail.com>
2024-06-16 00:01:50 +0530

    new code2

---
+++ JS/4_DataTypeConversion.js
@@ -0,0 +1,47 @@
+let age= undefined
+let myAge= Number (age)
+
+
+//console.log(age)
+//console.log(myAge)
+
+//console.log(typeof age)
+//console.log(typeof myAge)
+
+//'20'-> 20
+//'20abc' -> NaN
+//true-> 1 false-> 0
+
+//----to Boolean
+
+let isHuman= false
+let num1= String(isHuman)
+
+//console.log(num1)
+//console.log(typeof num1)
+
+let value=12
+let negValue= -value
+
+//console.log(negValue) //op- -12
+
+// console.log("1"+2+2);
+// 122
+// console.log(1+'2'+2);
+// 122
+// console.log(1+2+'2');
+// 32
+
+// let x=0
+// let y=99
+
+// console.log(--x);
+// console.log(--y);
+// console.log(x);
+// console.log(y);
+
+// let i= Symbol('890')
+// console.log(i);
+
+// console.log(i==Symbol(890))
+
